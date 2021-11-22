53abe214c4465cab53ab233d300067b9843865bf
JoJoBboott <94870625+JoJoBboott@users.noreply.github.com>
2021-11-22 22:01:36 +0100

    Update cmd.js

--- DataBase/cmd.js
+++ DataBase/cmd.js
@@ -1,7 +1,4 @@
-/* Codded by @Ravindu Manoj
-
-Telegram: t.me/RavinduManoj
-Facebook: https://www.facebook.com/ravindu.manoj.79
+/* Codded by JoJo
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
