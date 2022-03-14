86f081837fdaf2bcd1621764ce475ed2e5d1442e
✬Ravindu Manoj✬ ✓ <88134633+ravindu01manoj@users.noreply.github.com>
2022-03-14 10:57:34 +0530

    Update db.js

--- DataBase/db.js
+++ DataBase/db.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
-
+var mongo = '';
+if(process.env.MONGO_DB_URI) mongo = process.env.MONGO_DB_URI;
 module.exports = {
     MONGO_DB:true,
-    MONGOURL: process.env.MONGO_DB_URI === undefined ? '' : process.env.MONGO_DB_URI
+    MONGOURI: mongo
   }
